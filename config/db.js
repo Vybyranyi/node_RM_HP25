8ba5e5f635238c74abb5da98103e1b86f7ba8a05
Vybyranyi <mariankvit@gmail.com>
2026-04-02 19:01:10 +0300

    update database configuration to use environment variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345678",
-  database: "tasks",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 db.connect((error) => {
