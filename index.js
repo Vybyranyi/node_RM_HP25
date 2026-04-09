d4b598871c82816fdc1dfdd94cd90aa2de57e8f9
Vybyranyi <mariankvit@gmail.com>
2026-04-09 19:37:59 +0300

    add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
-import tasksRouter from './routes/tasksRoutes.js'
-import { logger } from "./middlewares/logger.js"
-// import { checkAuth } from "./middlewares/checkAuth.js"
+import tasksRouter from "./routes/tasksRoutes.js";
+import { logger } from "./middlewares/logger.js";
+import cors from "cors";
 
 const PORT = 3000;
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use(logger);
-// app.use(checkAuth);
-app.use("/tasks", tasksRouter)
+app.use("/tasks", tasksRouter);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущено на порту http://localhost:${PORT}`);
