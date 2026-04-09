156cab8241f5cbde9a901502ab9839b29edc68f9
Vybyranyi <mariankvit@gmail.com>
2026-04-09 19:47:16 +0300

    feat: add rate limiting middleware to enhance request handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import tasksRouter from "./routes/tasksRoutes.js";
 import { logger } from "./middlewares/logger.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const PORT = 3000;
 const app = express();
@@ -11,6 +12,16 @@ app.use(
     origin: ["http://localhost:3000", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+  }),
+);
+
+app.use(
+  rateLimit({
+    windowMs: 1 * 60 * 1000, // 15 хвилин
+    max: 100, // обмеження на 100 запитів з однієї IP-адреси за 15 хвилин
+    message: {
+      error: "Забагато запитів з цієї IP-адреси. Спробуйте пізніше.",
+    },
   }),
 );
 
